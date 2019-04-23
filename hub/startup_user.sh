b24a86681f2ca96db67aee75a4a2cd3196e91ea0
grouplightning <tianaleo1@gmail.com>
2019-04-23 14:39:55 -0500

    fix startup scripts

--- hub/startup_user.sh
+++ hub/startup_user.sh
@@ -2,5 +2,7 @@
 #commands and services that need to be ran as the user after hub startup
 #the path to this script must be added to configuration that will run it *after* the desktop environment is loaded
 #for example, for LXDE, you would add it to /etc/xdg/lxsession/LXDE/autostart
-python3 /home/pi/PRUC/scheduler.py &
-python3 /home/pi/PRUC/ui/ui.py &
+screen -dmS scheduler bash -c 'cd /home/pi/PRUC/hub/; python3 /home/pi/PRUC/hub/scheduler.py'
+
+cd /home/pi/PRUC/hub/ui/
+python3 /home/pi/PRUC/hub/ui/ui.py &
